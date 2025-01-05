1c72890d927bedf0cf88d44b521eea1b81d80f9c
Halil İbrahim Direktör <hidirektor@gmail.com>
2025-01-05 07:15:51 +0300

    Shortcut, Startup & Open External App

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module me.t3sl4.util.os {
     requires org.json;
     requires java.desktop;
     requires mslinks;
+    requires java.prefs;
 
     exports me.t3sl4.util.os;
     exports me.t3sl4.util.os.desktop;
